0bfb328b8bc97407048853d9bfb0d88e3f7d1d00
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-02-28 21:16:37 -0500

    -m "chore: created errors middleware"

--- API/app.js
+++ API/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const createError = require('http-errors')
 const express = require('express');
 const logger = require('morgan')
 
@@ -7,11 +8,24 @@ require('./configs/db.config')
 
 const app = express();
 
+app.use(express.json())
 app.use(logger('dev'))
 
 const api = require('./configs/routes.config')
 app.use('/v1', api)
 
+app.use((req, res, next) => next(createError(404, 'Route not found')));
+
+app.use((error, req, res, next) => {
+
+  const data = {
+    message: "Route not found"
+  }
+  res.status(error.status).json(data)
+
+})
+
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.info(`Application running in port ${port}`))
+app.listen(port, () => 
+console.info(`Application running in port ${port}`))
