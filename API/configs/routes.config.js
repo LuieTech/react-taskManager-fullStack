c7f620e223d52aa6b51d8bd75deb20850e778bfc
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-06 18:56:16 -0500

    "refactor: Implement new route for deleting groups"

--- API/configs/routes.config.js
+++ API/configs/routes.config.js
@@ -4,6 +4,7 @@ const groups = require('../controllers/task-group.controller')
 
 router.get('/task-groups', groups.list)
 router.post('/task-groups', groups.create)
+router.delete('/task-groups/:id', groups.delete)
 
 
 module.exports = router;
