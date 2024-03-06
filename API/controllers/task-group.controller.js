daf17c81abf550a95b59f3d20f58c2df4ab4f741
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-06 18:57:04 -0500

    "refactor: Implement 3 different versions of delete controller including while using async/await for training purposes"

--- API/controllers/task-group.controller.js
+++ API/controllers/task-group.controller.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors')
 const TaskGroup = require('../models/task-group.models');
 
 module.exports.list = (req, res, next) => {
@@ -18,6 +19,45 @@ module.exports.create = (req, res, next) => {
       res.status(201).json(group)
     })
     .catch(error => next(error))
-    
+
+}
+
+module.exports.delete = async (req, res, next) => {
+
+    try{
+      const group = await TaskGroup.findOne({ _id: req.params.id })
+        if(!group){
+          next(createError(404, "Taskgroup not found"));
+        } else {
+          await TaskGroup.deleteOne({ _id: req.params.id })
+          res.status(204).send()
+        }
+    } catch (error){
+        next(error)
+    }
+
+  // TaskGroup.findOne({ _id: req.params.id })
+  //   .then((group) => {
+  //     // console.log(group)
+  //     if(!group){
+  //       next(createError(404, "TaskGroup not found"))
+  //     } else {
+  //       return TaskGroup.deleteOne({ _id: req.params.id })
+  //         .then(() => res.status(204).send())
+  //     }
+  //   })
+  //   .catch(error => next(error))
+
+
+  // TaskGroup.findByIdAndDelete(req.params.id)
+  //   .then((group) => {
+  //     if(!group){
+  //       next(createError(404, 'TaskGroup not found'))
+  //     } else {
+  //       res.status(204).send() 
+  //     }
+      
+  //   })
+  //   .catch(error => next(error))
 
 }
