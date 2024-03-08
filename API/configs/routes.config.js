56d4e256235333621599bf50c8896ceebd892f4a
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-07 19:29:11 -0500

    chore: "implemented details search route path"

--- API/configs/routes.config.js
+++ API/configs/routes.config.js
@@ -5,6 +5,7 @@ const groups = require('../controllers/task-group.controller')
 router.get('/task-groups', groups.list)
 router.post('/task-groups', groups.create)
 router.delete('/task-groups/:id', groups.delete)
+router.get('/task-groups/:id', groups.detail)
 
 
 module.exports = router;
