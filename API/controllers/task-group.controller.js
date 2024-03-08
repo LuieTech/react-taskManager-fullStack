b07b08ff738bd5b47f5d8e1bd58c9539828e2c65
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-07 19:30:14 -0500

    chore: "implemented details search controller and a conditional req.query search logic "

--- API/controllers/task-group.controller.js
+++ API/controllers/task-group.controller.js
@@ -3,7 +3,14 @@ const TaskGroup = require('../models/task-group.models');
 
 module.exports.list = (req, res, next) => {
 
-  TaskGroup.find()
+  // const name = req.query && req.query.name ? req.query.name : "" ;
+  // const criteria = name ? { name: new RegExp(name, 'i') } : {};
+
+  const { name } = req.query
+  const criteria = {} 
+  if(name) criteria.name = name
+
+  TaskGroup.find( criteria )
     .then(group => res.json(group))
     .catch(error => next(error))
 
@@ -25,7 +32,7 @@ module.exports.create = (req, res, next) => {
 module.exports.delete = async (req, res, next) => {
 
     try{
-      const group = await TaskGroup.findOne({ _id: req.params.id })
+      const group = await TaskGroup.findOne({ _id: req.params.id }) /* VERSION WITH ASYNC */
         if(!group){
           next(createError(404, "Taskgroup not found"));
         } else {
@@ -36,7 +43,7 @@ module.exports.delete = async (req, res, next) => {
         next(error)
     }
 
-  // TaskGroup.findOne({ _id: req.params.id })
+  // TaskGroup.findOne({ _id: req.params.id }) =>  VERSION WITH .findOne
   //   .then((group) => {
   //     // console.log(group)
   //     if(!group){
@@ -49,7 +56,7 @@ module.exports.delete = async (req, res, next) => {
   //   .catch(error => next(error))
 
 
-  // TaskGroup.findByIdAndDelete(req.params.id)
+  // TaskGroup.findByIdAndDelete(req.params.id) => VERSION WITH .findByIdAndDelete
   //   .then((group) => {
   //     if(!group){
   //       next(createError(404, 'TaskGroup not found'))
@@ -59,5 +66,16 @@ module.exports.delete = async (req, res, next) => {
       
   //   })
   //   .catch(error => next(error))
+
+}
+
+module.exports.detail = (req, res, next) => {
+
+  TaskGroup.findById(req.params.id)
+    .then(group => {
+      if(!group) next(createError(404, "TaskGroup not found"))
+      else res.status(200).json(group)
+    })
+    .catch(error => next(error))
 
 }
