54fd316402e0d35295b3c6a89f00f7b7fa0ccb09
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-11 19:09:33 -0400

    chore: "implement routes for each task create and delete"

--- API/configs/routes.config.js
+++ API/configs/routes.config.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router();
 const groups = require('../controllers/task-group.controller')
+const tasks = require('../controllers/tasks.controller')
 
 router.get('/task-groups', groups.list)
 router.post('/task-groups', groups.create)
 router.delete('/task-groups/:id', groups.delete)
 router.get('/task-groups/:id', groups.detail)
+
+//Task routes
+
+router.post('/task-groups/:groupId/tasks', tasks.create)
+router.delete('/tasks/:id', tasks.delete)
+// '/task-groups/:groupId/tasks/:id' 
+
+
+
 
 
 module.exports = router;
